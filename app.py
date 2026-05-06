890ef0c83f49f98da2af0f3ac37133c3da2cbee0
Amir <a.mazhary1999@gmail.com>
2026-05-07 01:12:44 +0200

    Entries Route und JSON Antwort hinzugefügt

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Journal API läuft"
+
+@app.route("/entries")
+def get_entries():
+
+        entries = [
+            {
+                "id": 1,
+                "title": "Mein erster Eintrag",
+                "content": "Heute habe ich Flask verstanden"
+            },
+            {
+                "id": 2,
+                "title": "Backend Lernen",
+                "content": "APIs sind interessant"
+            }
+        ]
+
+        return jsonify(entries)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
