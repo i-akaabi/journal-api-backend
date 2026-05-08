f70482077b79664973c5ec150fdbee42cf13f16e
Amir <a.mazhary1999@gmail.com>
2026-05-09 00:25:08 +0200

    Neue Journal Einträge über POST Requests speicherbar gemacht

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import sqlite3
 
 app = Flask(__name__)
@@ -50,6 +50,32 @@ def get_entries():
     connection.close()
 
     return jsonify(entries)
+
+@app.route("/entries", methods=["POST"])
+def create_entry():
+
+    data = request.get_json()
+
+    title = data["title"]
+
+    content = data["content"]
+
+    connection = sqlite3.connect("database.db")
+
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        INSERT INTO entries (title, content)
+        VALUES (?, ?)
+    """, (title, content))
+
+    connection.commit()
+
+    connection.close()
+
+    return jsonify({
+    "message": "Neuer Eintrag erstellt"
+})
 
 
 if __name__ == "__main__":
